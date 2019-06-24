cb36d87abdd7c166e6c543df89847897c266b119
jboruar <rajen.boruah@jci.com>
2019-06-24 12:08:37 +0530

    Added multiplication function

--- test_calculator.py
+++ test_calculator.py
@@ -13,3 +13,6 @@ class TestCalculator:
 
     def test_subtraction(self):
         assert 2 == calculator.subtract(4, 2)
+
+    def test_multilication(self):
+        assert 6 == calculator.multiplication(2, 3)
